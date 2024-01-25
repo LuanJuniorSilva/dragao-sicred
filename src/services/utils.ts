d5e8d001c53e1419d75825aecce6f6b23398e732
luan jr <luan.juniorght7@gmail.com>
2024-01-24 21:50:12 -0300

    Removed url for login

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
-const apiLogin = axios.create({
-  baseURL: 'https://apitestedragon.herokuapp.com/',
-})
-
 const apiDefault = axios.create({
   baseURL: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon',
 })
 
-export { apiLogin, apiDefault }
+export { apiDefault }
